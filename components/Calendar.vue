a75f367229507f07eaba85e373d67e051781cc0a
ali <ali.zokaei.1367@gmail.com>
2025-02-11 22:12:38 +0330

    save

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -13,100 +13,75 @@
       </div>
     </div>
 
-    <div class="days-of-week text-center">
-      <span v-for="day in daysssOfWeek" :key="day">{{ day }}</span>
-    </div>
+
 
     <!-- Days of the Month -->
-    <div class="days-grid">
-
-      <!-- empty characters -->
-      <span v-for="day in emptyDays" :key="day">
-      </span>
-
-      <!-- start days characters -->
-      <span v-for="day in mainListDaysInMonth" :key="day" class="text-center relative day"
-        :class="[{ 'opacity-50 bg-gray-400': day.reserved, 'hover:bg-[#b3f9e9]': !day.reserved, 'bg-lime-300':day.isToday }]">
-        <div>{{ day.day }}</div>
-
-        <div class="flex justify-center items-center text-[10px] gap-1 text-white" dir="ltr">
-          <div v-if="day.discount" class="px-2 py-[2px] bg-rose-500 rounded-2xl ">
-            {{ day.discount }}
-            %
-          </div>
-          <div v-if="day.instantDiscount" class="px-2 py-[2px] bg-pink-400 rounded-2xl">{{ day.instantDiscount }} %
-          </div>
+    <div class="flex gap-4">
+      <div>
+        <div class="days-of-week text-center">
+          <span v-for="day in daysssOfWeek" :key="day">{{ day }}</span>
         </div>
-        <div v-if="day.lastMomentDiscount" class="absolute px-1 right-1 top-1 bg-green-300 rounded-full">a</div>
-      </span>
+        <div class="days-grid">
+
+          <!-- empty characters -->
+          <span v-for="day in emptyDays" :key="day">
+          </span>
+
+          <!-- start days characters -->
+          <span v-for="day in mainListDaysInMonth" :key="day" class="text-center relative day"
+            :class="[{ 'opacity-50 bg-gray-400': day.reserved, 'hover:bg-[#b3f9e9]': !day.reserved, 'bg-lime-300': day.isToday }]">
+            <div>{{ day.day }}</div>
+
+            <div class="flex justify-center items-center text-[10px] gap-1 text-white" dir="ltr">
+              <div v-if="day.discount" class="px-2 py-[2px] bg-rose-500 rounded-2xl ">
+                {{ day.discount }}
+                %
+              </div>
+              <div v-if="day.instantDiscount" class="px-2 py-[2px] bg-pink-400 rounded-2xl">{{ day.instantDiscount }} %
+              </div>
+            </div>
+            <div v-if="day.lastMomentDiscount" class="absolute px-1 right-1 top-1 bg-green-300 rounded-full">a</div>
+          </span>
+
+        </div>
+      </div>
+
+      <div>
+        <div class="days-of-week text-center">
+          <span v-for="day in daysssOfWeek" :key="day">{{ day }}</span>
+        </div>
+        <div class="days-grid">
+
+          <!-- empty characters -->
+          <span v-for="day in emptyDays" :key="day">
+          </span>
+
+          <!-- start days characters -->
+          <span v-for="day in mainListDaysInMonth" :key="day" class="text-center relative day"
+            :class="[{ 'opacity-50 bg-gray-400': day.reserved, 'hover:bg-[#b3f9e9]': !day.reserved, 'bg-lime-300': day.isToday }]">
+            <div>{{ day.day }}</div>
+
+            <div class="flex justify-center items-center text-[10px] gap-1 text-white" dir="ltr">
+              <div v-if="day.discount" class="px-2 py-[2px] bg-rose-500 rounded-2xl ">
+                {{ day.discount }}
+                %
+              </div>
+              <div v-if="day.instantDiscount" class="px-2 py-[2px] bg-pink-400 rounded-2xl">{{ day.instantDiscount }} %
+              </div>
+            </div>
+            <div v-if="day.lastMomentDiscount" class="absolute px-1 right-1 top-1 bg-green-300 rounded-full">a</div>
+          </span>
+
+        </div>
+      </div>
 
     </div>
+
   </div>
 </template>
 
 <script setup>
 import { toJalaali, jalaaliToDateObject, isLeapJalaaliYear, jalaaliMonthLength } from 'jalaali-js';
-
-const daysssOfWeek = ['ش', 'ی', 'د', 'س', 'چ', 'پ', 'ج',];
-const months = [
-  'فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور',
-  'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند',
-];
-
-const shamsiMonths = ref([
-  { name: 'فروردین', enName: 'farvardin', days: 31 },
-  { name: 'اردیبهشت', enName: 'ordibehesht', days: 31 },
-  { name: 'خرداد', enName: 'khordad', days: 31 },
-  { name: 'تیر', enName: 'tir', days: 31 },
-  { name: 'مرداد', enName: 'mordad', days: 31 },
-  { name: 'شهریور', enName: 'shahrivar', days: 31 },
-  { name: 'مهر', enName: 'mehr', days: 30 },
-  { name: 'آبان', enName: 'aban', days: 30 },
-  { name: 'آذر', enName: 'azar', days: 30 },
-  { name: 'دی', enName: 'dey', days: 30 },
-  { name: 'بهمن', enName: 'bahman', days: 30 },
-  { name: 'اسفند', enName: 'esfand', days: 29 } // 29 or 30 depending on leap year
-]);
-
-const emptyDays = ref(0)
-const indexMonthOfYear = ref(0)
-const mainListDaysInMonth = ref([])
-// GET TODAY 
-const toDay = ref({})
-const newDate = new Date();
-const showingDate = new Date(newDate);
-const showingDateJalali = ref({});
-
-
-const createShowingDate = (numberOfNextMonth) => {
-
-  toDay.value = toJalaali(newDate)
-  showingDate.setMonth(showingDate.getMonth() + numberOfNextMonth);
-  showingDateJalali.value = toJalaali(showingDate)
-
-  const dateObject = jalaaliToDateObject(showingDateJalali.value.jy, showingDateJalali.value.jm, 1);
-
-  emptyDays.value = convertToShamsiDayNumber(dateObject.getDay());
-  indexMonthOfYear.value = getShamsiMonthNumber(showingDate);
-
-  // if is leap year, Esfand have to be 30 days 
-  shamsiMonths.value[11].days = isLeapShowingYear.value ? 30 : 29
-
-
-  calculateDays()
-}
-
-
-function convertToShamsiDayNumber(gregorianDay) {
-  return (gregorianDay + 1) % 7;
-}
-
-
-
-const firstDayOfMonthShamsi = daysssOfWeek[emptyDays.value];
-
-
-
 const propertyDays = ref({
   bahman: {
     1: {
@@ -134,6 +109,59 @@ const propertyDays = ref({
     },
   },
 })
+
+
+const daysssOfWeek = ['ش', 'ی', 'د', 'س', 'چ', 'پ', 'ج',];
+
+const shamsiMonths = ref([
+  { name: 'فروردین', enName: 'farvardin', days: 31 },
+  { name: 'اردیبهشت', enName: 'ordibehesht', days: 31 },
+  { name: 'خرداد', enName: 'khordad', days: 31 },
+  { name: 'تیر', enName: 'tir', days: 31 },
+  { name: 'مرداد', enName: 'mordad', days: 31 },
+  { name: 'شهریور', enName: 'shahrivar', days: 31 },
+  { name: 'مهر', enName: 'mehr', days: 30 },
+  { name: 'آبان', enName: 'aban', days: 30 },
+  { name: 'آذر', enName: 'azar', days: 30 },
+  { name: 'دی', enName: 'dey', days: 30 },
+  { name: 'بهمن', enName: 'bahman', days: 30 },
+  { name: 'اسفند', enName: 'esfand', days: 29 } // 29 or 30 depending on leap year
+]);
+
+const emptyDays = ref(0)
+const indexMonthOfYear = ref(0)
+const mainListDaysInMonth = ref([])
+// GET TODAY 
+const toDay = ref({})
+const newDate = new Date();
+const showingDate = new Date(newDate);
+const showingDateJalali = ref({});
+
+toDay.value = toJalaali(newDate)
+
+const createShowingDate = (numberOfNextMonth) => {
+
+
+  showingDate.setMonth(showingDate.getMonth() + numberOfNextMonth);
+  showingDateJalali.value = toJalaali(showingDate)
+
+  const dateObject = jalaaliToDateObject(showingDateJalali.value.jy, showingDateJalali.value.jm, 1);
+
+  emptyDays.value = convertToShamsiDayNumber(dateObject.getDay());
+  indexMonthOfYear.value = getShamsiMonthNumber(showingDate);
+
+  // if is leap year, Esfand have to be 30 days 
+  shamsiMonths.value[11].days = isLeapShowingYear.value ? 30 : 29
+
+
+  calculateDays()
+}
+
+
+function convertToShamsiDayNumber(gregorianDay) {
+  return (gregorianDay + 1) % 7;
+}
+
 
 
 
